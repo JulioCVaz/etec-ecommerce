6f69b8edc90f47b66df64e78858f17a03a883798
JulioCVaz <julio.oliveiravaz0@gmail.com>
2018-05-30 00:08:35 -0300

    Layout Inicial

---
+++ cabecalho.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ETEC-ECOMMERCE</title>
+    <!-- fonts -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+    <!-- bootstrap and css -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style/style.css" type="text/css">
+    <!-- styles -->
+    
+</head>
+<body>
+    <section class="main">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="#"><i class="fas fa-shopping-cart"></i>ETECOMMERCE</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav navbar-content">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Produtos <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Carrinho</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contato</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="fas fa-sign-in-alt"></i>Login</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="container">
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+include('cabecalho.php');
+?>
+
+    <div class="row box-image">
+        <div class="col title-products">
+            <h3>Computadores</h3>
+            <hr>
+        </div>
+    </div>
+    <div class="row justify-content-md-around box-image">
+        <div class="col col-md-3 chromebook"></div>
+        <div class="col col-md-5 imac"></div>
+        <div class="col col-md-3 samsung"></div>
+    </div>
+<?php
+include('rodape.php');
