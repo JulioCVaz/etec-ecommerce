8ccfacf6d88c84d00f9abcc4eeba8bf1c0b67e9a
JulioCVaz <julio.oliveiravaz0@gmail.com>
2018-06-01 00:21:15 -0300

    criado ajax e manipulando dados no servidor(php)

--- carrinho.php
+++ carrinho.php
@@ -1,8 +1,16 @@
 <?php
+
 include('cabecalho.php');
+
+$produtos = json_decode($_POST['produto']);
+var_dump($produtos);
+$preco = json_decode($_POST['preco']);
+
 ?>
 
 <div>Hello World</div>
+<p><?php echo $produtos?></p>
+<p><?php echo $preco?></p>
 
 <?php
 include('rodape.php');
--- js/adiciona-produto.js
+++ js/adiciona-produto.js
@@ -12,6 +12,46 @@ var comprar = document.querySelectorAll('.buy');
 
 for (var i = 0; i < comprar.length; i++) {
 	comprar[i].addEventListener('click', function (event) {
-        console.log(event.target.parentNode);
+		
+		var span = event.target.parentNode;
+
+		var arrayContent = [];
+
+		var teste = span.querySelectorAll('span');
+
+		for(var y = 0; y < teste.length; y++){
+			var contentSpan = teste[y].innerHTML;
+			arrayContent.push(contentSpan);
+		}
+		console.log(span);
+		console.log(arrayContent);
+
+		var dados = arrayContent;
+		enviaDados(dados);
+	
+
+
+
 	}, false);
+}
+
+
+// ajax
+function enviaDados(dados){	
+	var http = new XMLHttpRequest();
+	var url = 'carrinho.php';
+	var produto = dados[0];
+	var preco = dados[1];
+	var content = "{'produto':'"+produto+"','preco':'"+preco+"'}";
+	
+	http.open('POST', url, true);
+	
+http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+http.onreadystatechange = function(){
+	if(http.readyState == 4 && http.status == 200){
+		console.log('enviei os dados');
+	}
+}
+http.send(content);
 }
